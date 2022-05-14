f7da68c697f5a738c2143a998e6512e550bedb25
Imran Khalid <imran.khalid.code1@gmail.com>
2022-05-14 08:58:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import OpenData from "./data/OpenDay.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="App">
+			{OpenData &&
+				OpenData.map((open) => {
+					return (
+						<div className="App" key={open.id}>
+							<strong>{open.description}</strong>
+							<br></br>
+							{open.start_time}
+							<br></br>
+
+							{open.topics &&
+								open.topics.map((data) => {
+									return <div key={open.id}>{data.description}</div>;
+								})}
+						</div>
+					);
+				})}
+		</div>
+	);
+};
 
 export default App;
